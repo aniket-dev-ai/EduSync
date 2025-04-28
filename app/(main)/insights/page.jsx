2fe4839ef4b1483b6a6b8eb518d3a355f9035e30
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-04-29 01:43:51 +0530

    Add Dashboard and Insights components with layout and data handling

--- app/(main)/insights/page.jsx
+++ app/(main)/insights/page.jsx
@@ -1,23 +1,22 @@
 import { getIndustryInsights } from "@/actions/dashboard";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
-import Dashboard from "./_components/Dashboard";
+import DashboardView from "./_components/DashboardView";
 import { getAssessments } from "@/actions/interview";
 
 const IndustryInsightsPage = async () => {
     const {isOnboarded} = await getUserOnboardingStatus();
     const assessments = await getAssessments();
 
-    // if(!isOnboarded){
-    //     redirect("/onboarding");
-    //     return null; 
-    // }
+    if(!isOnboarded){
+        redirect("/onboarding");
+        return null; 
+    }
     // first user is onboarded then get the industryInsights
-    // const insights = await getIndustryInsights();
+    const insights = await getIndustryInsights();
     return (
         <div className="container mx-auto"> 
-            {/* <Dashboard insights={insights} assessments={assessments} /> */}
-            <Dashboard />
+            <DashboardView insights={insights} assessments={assessments} />
         </div>
     );
 };
